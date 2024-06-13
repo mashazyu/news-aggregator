59c8041fc97cc92348868898bd9748d9394ee230
Maria Zadnepryanets <115021350+mariadoggo@users.noreply.github.com>
2024-06-13 12:39:25 +0200

    feat: filter removed articles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import Article from "./components/Article";
 import { getArticles } from "./api/articles";
+import { filterRemovedArticles } from "./lib/utils";
 
 function App() {
   const articlesQuery = useQuery({
@@ -10,10 +11,12 @@ function App() {
   });
 
   const { data, isLoading, isError, error } = articlesQuery;
-  const { articles } = data;
 
   if (isLoading) return <h1>loading...</h1>;
   if (isError) return <h1>{JSON.stringify(error)}</h1>;
+
+  // Some articles might be removed, so I filter those articles out
+  const articles = filterRemovedArticles(data.articles);
 
   return (
     <main className="container mx-auto">
