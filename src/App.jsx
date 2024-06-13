048968441f6257cdee70f6cb508b024551ec26d9
Maria Zadnepryanets <115021350+mariadoggo@users.noreply.github.com>
2024-06-13 12:22:02 +0200

    feat: install card & add Article component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { Button } from "@/components/ui/button";
+
+import Article from "./components/Article";
 
 const ARTICLES = [
-  {
-    source: {
-      id: null,
-      name: "[Removed]",
-    },
-    author: null,
-    title: "[Removed]",
-    description: "[Removed]",
-    url: "https://removed.com",
-    urlToImage: null,
-    publishedAt: "1970-01-01T00:00:00Z",
-    content: "[Removed]",
-  },
   {
     source: {
       id: "google-news",
@@ -43,6 +31,40 @@ const ARTICLES = [
     publishedAt: "2024-06-07T08:10:56Z",
     content: null,
   },
+  {
+    source: {
+      id: null,
+      name: "CNBC",
+    },
+    author: "Lim Hui Jie",
+    title:
+      "Asia markets mixed as Japan spending data misses estimates, China's May exports top expectations - CNBC",
+    description:
+      "China's May exports beat expectations, climbing 7.6% against the 6% expected by a Reuters poll of economists and vastly higher than the 1.5% rise seen in April.",
+    url: "https://www.cnbc.com/2024/06/07/asia-markets-live.html",
+    urlToImage:
+      "https://image.cnbcfm.com/api/v1/image/107290834-1692808192061-gettyimages-1619309256-fc_1099083.jpeg?v=1694043910&w=1920&h=1080",
+    publishedAt: "2024-06-07T07:58:00Z",
+    content:
+      "A view of the automated container port in Qingdao in east China's Shandong province. \r\nAsia-Pacific stocks were mixed on Friday as investors looked at economic data from China and digested Japan's ho… [+2389 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "CNBC",
+    },
+    author: "Sam Meredith",
+    title:
+      "Credit Suisse bondholders sue Switzerland in the U.S. over $17 billion writedown of AT1 debt - CNBC",
+    description:
+      "A group of Credit Suisse bondholders have filed a lawsuit over the Swiss government's decision to write down the failed bank's Additional Tier 1 debt.",
+    url: "https://www.cnbc.com/2024/06/07/credit-suisse-bondholders-sue-switzerland-over-17-billion-at1-wipeout.html",
+    urlToImage:
+      "https://image.cnbcfm.com/api/v1/image/107215099-1679694376490-gettyimages-1249282050-AFP_33BZ7HC.jpeg?v=1717740943&w=1920&h=1080",
+    publishedAt: "2024-06-07T07:23:32Z",
+    content:
+      "A group of Credit Suisse bondholders filed a lawsuit against the Swiss government, seeking full compensation over the contentious decision to write down the failed bank's Additional Tier 1 (AT1) debt… [+2061 chars]",
+  },
 ];
 
 function App() {
@@ -51,15 +73,19 @@ function App() {
     queryFn: () => Promise.resolve([...ARTICLES]),
   });
 
-  const { isLoading, isError, error } = articlesQuery;
+  const { data: articles, isLoading, isError, error } = articlesQuery;
 
   if (isLoading) return <h1>loading...</h1>;
   if (isError) return <h1>{JSON.stringify(error)}</h1>;
 
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <main className="container mx-auto">
+      <div className="grid grid-cols-3 gap-8">
+        {articles.map((article) => (
+          <Article article={article} key={article.title} />
+        ))}
+      </div>
+    </main>
   );
 }
 
