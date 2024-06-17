d0fd56914eb6af37435768242b6285b37d7c1e20
Maria Zadnepryanets <115021350+mariadoggo@users.noreply.github.com>
2024-06-17 06:49:18 +0200

    feat: add infinite scroll

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,31 +1,61 @@
 import PropTypes from "prop-types";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
+import { Button } from "./ui/button";
 import Article from "./Article";
 import { getArticles } from "../api/articles";
 import { filterRemovedArticles } from "../lib/utils";
 
+import { PAGE_SIZE } from "../constants";
+
 function Articles({ category, query }) {
-  const articlesQuery = useQuery({
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     queryKey: ["articles", category, query],
-    queryFn: () => getArticles({ category, query }),
+    queryFn: ({ pageParam }) =>
+      getArticles({ category, page: pageParam, query }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.totalResults <= pages.length * PAGE_SIZE) return null;
+
+      return pages.length;
+    },
   });
 
-  const { data, isLoading, isError, error } = articlesQuery;
+  return status === "pending" ? (
+    <p>Loading...</p>
+  ) : status === "error" ? (
+    <p>Error: {error.message}</p>
+  ) : (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 py-8">
+        {data.pages.map((currentPage) => {
+          // Api returns entries with [Removed] in title and content.
+          // Below I am filtering out those entries.
+          const articles = filterRemovedArticles(currentPage.articles);
 
-  if (isLoading) return <h1>loading...</h1>;
-  if (isError) return <h1>{JSON.stringify(error)}</h1>;
-
-  // Api returns entries with [Removed] in title and content.
-  // Below I am filtering out those entries.
-  const articles = filterRemovedArticles(data.articles);
-
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 py-8">
-      {articles.map((article) => (
-        <Article article={article} key={article.title} />
-      ))}
-    </div>
+          return articles.map((article) => (
+            <Article article={article} key={article.title} />
+          ));
+        })}
+      </div>
+      {hasNextPage && (
+        <Button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage ? "Loading more..." : "Load More"}
+        </Button>
+      )}
+      <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
+    </>
   );
 }
 
