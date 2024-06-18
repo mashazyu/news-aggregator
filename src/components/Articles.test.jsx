86630189bd60ac2d36fada4066c6ee969a8b1f40
Maria Zadnepryanets <115021350+mariadoggo@users.noreply.github.com>
2024-06-18 14:45:04 +0200

    chore: substitute nock with vi.spyOn

--- src/components/Articles.test.jsx
+++ src/components/Articles.test.jsx
@@ -1,23 +1,21 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
-import nock from "nock";
 
 import { wrapper } from "../../tests/utils";
 import { articlesMock as articles } from "../../tests/mocks";
 import Articles from "./Articles";
+import axios from "axios";
 
-nock("https://newsapi.org")
-  .defaultReplyHeaders({
-    "access-control-allow-origin": "*",
-    "access-control-allow-credentials": "true",
+vi.spyOn(axios, "get").mockImplementation(() =>
+  Promise.resolve({
+    data: {
+      articles,
+      isLoading: false,
+      isError: false,
+      error: null,
+    },
   })
-  .get((uri) => uri.includes("/v2/top-headlines"))
-  .reply(200, {
-    articles,
-    isLoading: false,
-    isError: false,
-    error: null,
-  });
+);
 
 describe("Articles", () => {
   it("renders articles", async () => {
