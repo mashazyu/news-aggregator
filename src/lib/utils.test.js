bf7a37a4f44b295bc7ba0b1df46099939758229c
Maria Zadnepryanets <115021350+mariadoggo@users.noreply.github.com>
2024-06-21 09:18:33 +0200

    test: add more tests for filterRemovedArticles()

--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -2,19 +2,17 @@ import { describe, it, expect } from "vitest";
 
 import { filterRemovedArticles } from "./utils";
 
-describe("filterRemovedArticles", () => {
-  it("true to be true", () => {
-    const articles = [
-      {
-        title: "[Removed]",
-      },
-      {
-        title: "regular title",
-      },
-      {
-        title: "title contains word Removed",
-      },
-    ];
-    expect(filterRemovedArticles(articles)).toHaveLength(2);
-  });
+describe("filterRemovedArticles()", () => {
+  it.each`
+    articles                                                                      | filteredArticles
+    ${[{ title: "[Removed]" }, { title: "[Removed]" }]}                           | ${[]}
+    ${[{ title: "[Removed]" }, { title: "Removed" }]}                             | ${[{ title: "Removed" }]}
+    ${[]}                                                                         | ${[]}
+    ${[{ title: "Title with removed" }, { title: "Another title with Removed" }]} | ${[{ title: "Title with removed" }, { title: "Another title with Removed" }]}
+  `(
+    "filters $articles -> $filteredArticles",
+    ({ articles, filteredArticles }) => {
+      expect(filterRemovedArticles(articles)).toEqual(filteredArticles);
+    }
+  );
 });
