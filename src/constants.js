983ceb836f429503fdf7ded22c624e119db0687a
Maria Zadnepryanets <115021350+mariadoggo@users.noreply.github.com>
2024-06-24 09:03:39 +0200

    chore: move API key to .env file and gh repo secret

--- src/constants.js
+++ src/constants.js
@@ -10,4 +10,4 @@ export const CATEGORIES = [
 
 export const PAGE_SIZE = 9;
 export const ENDPOINT = "https://newsapi.org/v2/top-headlines";
-export const API_KEY = "apiKey";
+export const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
