576aded9336dc7b57c2f32478c0280787682ead0
Maria Zadnepryanets <115021350+mariadoggo@users.noreply.github.com>
2024-06-26 12:01:34 +0200

    feat: display article category

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -9,18 +9,31 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "./ui/button";
+import { Badge } from "@/components/ui/badge";
 
 import { isCreatorAvailable } from "../lib/utils";
 
 function Article({ article }) {
-  const { description, link, creator, title } = article;
-
+  const {
+    article_id: id,
+    category,
+    creator,
+    description,
+    link,
+    title,
+  } = article;
+  console.log("// article ", article);
   return (
     <Card className="flex flex-col justify-between">
       <CardHeader>
         <CardTitle>{title}</CardTitle>
         {isCreatorAvailable(creator) && (
           <CardDescription>by {creator[0]}</CardDescription>
+        )}
+        {category.length > 0 && (
+          <Badge key={category + id} className="max-w-min">
+            {category}
+          </Badge>
         )}
       </CardHeader>
       <CardContent>
@@ -39,6 +52,8 @@ function Article({ article }) {
 
 Article.propTypes = {
   article: PropTypes.shape({
+    article_id: PropTypes.string,
+    category: PropTypes.array,
     creator: PropTypes.array,
     title: PropTypes.string,
     description: PropTypes.string,
